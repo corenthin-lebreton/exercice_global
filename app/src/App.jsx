c025652da67e54ae24cf8971f4cf9bcb84875134
root <root@Ansible.firewax.fr>
2025-09-25 14:45:01 +0000

    initial commit

---
+++ app/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <div class="container">
+          <h1>My Todo App</h1>
+          <form id="todo-form">
+              <input type="text" id="todo-input" placeholder="Add a new task..." autocomplete="off" />
+              <button type="submit">Add</button>
+          </form>
+          <ul id="todo-list"></ul>
+      </div>
+    </>
+  )
+}
+
+export default App
